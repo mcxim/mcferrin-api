f84677939fed60e5c24ec5df2071824779d41c13
Maxim Gelfand <gelfandmaxim@gmail.com>
2020-05-08 22:18:26 +0300

    Add jwts

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
+import jwt
+import datetime
+from functools import wraps
+import base64
+import binascii
+from pprint import pprint
+
 import os
 
 # Init app
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
+app.config[
+    "SECRET_KEY"
+] = "\xa6\xc6\xc4\xa8\xcb\x19Z\x9f\xd4\xf3\xe7\xf1\xa6b\xe8\xf5`+\xa2\xf0\x8aO\xe3\xa6"
 # Database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
     basedir, "db.sqlite"
@@ -19,11 +31,15 @@ ma = Marshmallow(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
+    public_id = db.Column(db.String)
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
     data = db.Column(db.String())
 
-    def __init__(self, username, data=""):
+    def __init__(self, public_id, username, password, data=""):
+        self.public_id = public_id
         self.username = username
+        self.password = password
         self.data = data
 
 
@@ -32,65 +48,150 @@ class UserSchema(ma.SQLAlchemySchema):
         model = User
 
     id = ma.auto_field()
+    public_id = ma.auto_field()
     username = ma.auto_field()
+    password = ma.auto_field()
     data = ma.auto_field()
 
 
+class PublicUserSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = User
+
+    public_id = ma.auto_field()
+    username = ma.auto_field()
+
+
 user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+users_schema = PublicUserSchema(many=True)
+all_info_schema = UserSchema(many=True)
 
 
-@app.route("/user", methods=["POST"])
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        print("requiring token!")
+        token = None
+        if "x-access-tokens" in request.headers:
+            token = request.headers["x-access-tokens"]
+        if not token:
+            return jsonify({"Error": "a valid tolkien is missing"}), 401
+        try:
+            print(token)
+            data = jwt.decode(token, app.config["SECRET_KEY"])
+            print(data)
+            current_user = User.query.filter_by(public_id=data["public_id"]).first()
+        except Exception as e:
+            return jsonify({"Error": "tolkien is invalid " + str(e)}), 401
+        return f(current_user, *args, **kwargs)
+
+    return decorator
+
+
+@app.route("/register", methods=["POST"])
 def create_user():
     print("request.json: " + str(request.json))
     if not request.json:
-        return jsonify({"Error": "request json is present."}), 403
+        return jsonify({"Error": "request json is not present."}), 400
     missing_fields = [
-        field for field in ["data", "username"] if field not in request.json
+        field for field in ["username", "password"] if field not in request.json
     ]
     if missing_fields:
         return jsonify(
             {"Error": "The following fields are missing: " + str(missing_fields)}
         )
     username = request.json["username"]
-    data = request.json["data"]
+    hex_login = request.json["password"]
     if User.query.filter_by(username=username).first() != None:
-        return (
-            jsonify({"Error": "This username already exists in the database."}),
-            403,
-        )
-    new_user = User(username, data)
+        return jsonify({"Error": "This username already exists in the database."}), 400
+    try:
+        login_hash = bytes.fromhex(hex_login)
+    except ValueError:
+        return jsonify({"Error": "Invalid hexadecimal login hash"})
+    stored_hash = generate_password_hash(login_hash, method="pbkdf2:sha256:100100")
+    new_user = User(
+        public_id=str(uuid.uuid4()), username=username, password=stored_hash, data=""
+    )
     db.session.add(new_user)
     db.session.commit()
-    return user_schema.jsonify(new_user), 201
+    return (jsonify({"OK": "Success"}), 201)
+
+
+@app.route("/login", methods=["GET"])
+def login_user():
+    print(request.headers["Authorization"])
+    auth = request.authorization
+    print(auth)
+    if not auth or not auth.username or not auth.password:
+        return jsonify({"Error": "Could not verify - Login required"}, 401)
+    try:
+        login_hash = bytes.fromhex(auth.password)
+    except ValueError:
+        return jsonify({"Error": "Invalid hexadecimal login hash"})
+    user = User.query.filter_by(username=auth.username).first()
+    if check_password_hash(user.password, login_hash):
+        token = jwt.encode(
+            {
+                "public_id": user.public_id,
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+            },
+            app.config["SECRET_KEY"],
+        )
+        return jsonify({"token": token.decode("UTF-8")})
+    return jsonify({"Error": "Invalid password."}), 401
 
 
 @app.route("/user", methods=["GET"])
-def get_all_users():
-    return jsonify(users_schema.dump(User.query.all())), 200
+@token_required
+def get_user(user):
+    return user_schema.jsonify(user), 200
 
 
-@app.route("/user/<id>", methods=["GET"])
-def get_user(id):
-    return user_schema.jsonify(User.query.get(id)), 200
+@app.route("/user/data", methods=["GET"])
+@token_required
+def get_user_data(user):
+    return jsonify({"data": user.data})
 
 
-@app.route("/user/<id>", methods=["PUT"])
-def update_user(id):
-    user = User.query.get(id)
-    for field in request.json:
-        if request.json[field] != " ":
-            setattr(user, field, request.json[field])
+@app.route("/user/data", methods=["PUT"])
+@token_required
+def update_data(user):
+    print(request.data)
+    user.data = request.data.decode("ascii")
+    return ("", 204)
+
+@app.route("/user", methods=["DELETE"])
+@token_required
+def delete_user(user):
+    db.session.delete(user)
     db.session.commit()
-    return user_schema.jsonify(user)
+    return ("", 204)
 
-
-@app.route("/user/<id>", methods=["DELETE"])
-def delete_user(id):
+@app.route("/admin/<id>", methods=["DELETE"])
+def admin_delete_user(id):
     user = User.query.get(id)
     db.session.delete(user)
     db.session.commit()
     return user_schema.jsonify(user)
+
+
+@app.route("/admin", methods=["GET"])
+def all_info():
+    return all_info_schema.jsonify(User.query.all())
+
+
+@app.route("/admin/<id>", methods=["PUT"])
+def admin_put(id):
+    for field in request.json:
+        setattr(user, field, request.json[field])
+    db.session.commit()
+    return user_schema.jsonify(user), 202
+
+
+@app.route("/protected", methods=["GET"])
+@token_required
+def sample_protected(user):
+    return "how did you get here, " + str(user.__dict__["username"]) + "?"
 
 
 if __name__ == "__main__":
